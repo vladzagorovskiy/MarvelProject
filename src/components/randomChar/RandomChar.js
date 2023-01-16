5791000d0a618937aa2fbb579d1851b812373b65
vlad <zagorovskiy98@gmail.com>
2023-01-16 13:58:40 +0200

    component did mound( fix bag with 2 update)

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -9,7 +9,6 @@ import mjolnir from '../../resources/img/mjolnir.png';
 class RandomChar extends Component {
     constructor(props) {
         super(props);
-        this.updateChar();
     }
 
     state = {
@@ -19,6 +18,15 @@ class RandomChar extends Component {
     }
 
     marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.updateChar();
+        // this.timerId = setInterval(this.updateChar, 3000);
+    }
+
+    componentWillUnmount() {
+        // clearInterval(this.timerId);
+    }
 
     onCharLoaded = (char) => {
         this.setState({
