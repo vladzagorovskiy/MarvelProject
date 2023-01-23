26f2d03460969c403364e9f6067f928f8815ab54
vlad <zagorovskiy98@gmail.com>
2023-01-23 17:00:19 +0200

    Fix length, thumbnail and no data in CharInfo

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -84,10 +84,15 @@ class CharInfo extends Component {
 
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki, comics} = char;
+
+    let imgStyle = {'objectFit' : 'cover'};
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'contain'};
+    }
     return (
         <>
             <div className="char__basics">
-                <img src={thumbnail} alt={name}/>
+                <img src={thumbnail} alt={name} style={imgStyle}/>
                     <div>
                         <div className="char__info-name">{name}</div>
                         <div className="char__btns">
@@ -105,9 +110,9 @@ const View = ({char}) => {
                     </div>
                     <div className="char__comics">Comics:</div>
                     <ul className="char__comics-list">
-
+                    {comics.length > 0 ? null : 'There is no comics with  this hero'}
                     {
-                        comics.map((item, i) => {
+                        comics.slice(0, 10).map((item, i) => {
                             return (
                                 <li className="char__comics-item">
                                 {item.name}
@@ -118,7 +123,7 @@ const View = ({char}) => {
     
                     </ul>
         </>
-    )
+    ) 
 }
 
 export default CharInfo;
