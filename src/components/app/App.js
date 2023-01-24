e326395e4b4fc07aec3bdbd4fdb6c21605e290ce
vlad <zagorovskiy98@gmail.com>
2023-01-24 16:09:52 +0200

    1

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -24,11 +24,15 @@ class App extends Component {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <RandomChar/>
+                    <ErrorBoundary>
+                        <RandomChar/>
+                    </ErrorBoundary>
                     <div className="char__content">
-                        <CharList onCharSelected={this.onCharSelected}/>
                         <ErrorBoundary>
-                        <CharInfo charId={this.state.selectedChar}/>
+                            <CharList onCharSelected={this.onCharSelected}/>
+                        </ErrorBoundary>
+                        <ErrorBoundary>
+                            <CharInfo charId={this.state.selectedChar}/>
                         </ErrorBoundary>
                     </div>
                     <img className="bg-decoration" src={decoration} alt="vision"/>
