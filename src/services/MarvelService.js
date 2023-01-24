8311640d94d5c99da505821b04bb50954e69e420
vlad <zagorovskiy98@gmail.com>
2023-01-24 16:10:22 +0200

    1

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -3,7 +3,7 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=627995bded75de2c8281e19d6f34c86b';
-
+    _baseOffset = 210;
 
     getResource = async (url) => {
         let res = await fetch(url);
@@ -15,8 +15,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
