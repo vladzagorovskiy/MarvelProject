cdd9f535be0a06a3fad5c50ce3bc520a19d34163
vlad <zagorovskiy98@gmail.com>
2023-02-02 12:57:12 +0200

    123

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 import App from './components/app/App';
-import MarvelService from './services/MarvelService';
+// import MarvelService from './services/MarvelService';
 
 import './style/style.scss';
 
